f8b08a8045fc164a386b4ca8a786b221b801ca02
MOROLARI <tmorolari@gmail.com>
2018-07-16 04:21:26 +0200

    #159061833 add unit test for authenticate endpoint

---
+++ src/test/route/v1/authenticate-endpoint.js
@@ -0,0 +1,63 @@
+// Require the dev-dependencies
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import bcrypt from 'bcryptjs';
+import server from '../../../main/app';
+import userRepository from '../../../main/repository/users';
+
+chai.use(chaiHttp);
+
+const should = chai.should();
+
+describe('Authentication API test', () => {
+  beforeEach((done) => {
+    userRepository.clear();
+    done();
+  });
+  describe('POST /api/v1/account/register Authenticate User', () => {
+    it('should fail authentication when invalid email or password is provided', (done) => {
+      userRepository.save({
+        email: 'example@local',
+        password: bcrypt.hashSync('topsecret', 8),
+        name: 'Jane Doe',
+      });
+      chai.request(server)
+        .post('/api/v1/authenticate')
+        .send({ password: 'fakepass', email: 'example@local' })
+        .end((err, res) => {
+          res.should.have.status(401);
+          res.body.should.be.a('object');
+          res.body.should.not.have.property('token');
+          res.body.should.have.property('auth').eql(false);
+        });
+
+      chai.request(server)
+        .post('/api/v1/authenticate')
+        .send({ password: 'topsecret', email: 'fakemail@local' })
+        .end((err, res) => {
+          res.should.have.status(401);
+          res.body.should.be.a('object');
+          res.body.should.have.property('auth').eql(false);
+          res.body.should.not.have.property('token');
+          done();
+        });
+    });
+    it('it should authenticate user a return a valid token', (done) => {
+      userRepository.save({
+        email: 'example@local',
+        password: bcrypt.hashSync('topsecret', 8),
+        name: 'Jane Doe',
+      });
+      chai.request(server)
+        .post('/api/v1/authenticate')
+        .send({ password: 'topsecret', email: 'example@local' })
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.a('object');
+          res.body.should.have.property('token');
+          res.body.should.have.property('auth').eql(true);
+          done();
+        });
+    });
+  });
+});
