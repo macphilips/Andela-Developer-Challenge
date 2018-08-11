354dda121ceebcda0994c9a7814c04ba8108ef66
MOROLARI <tmorolari@gmail.com>
2018-08-11 23:44:23 +0200

    bug(Make list all entries view UI responsive): fix cors issues

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -2,17 +2,5 @@ export default {
   secret: process.env.JWT_SECRET,
   validity: parseInt(process.env.TOKEN_VALIDITY, 10),
   nodeEnv: process.env.NODE_ENV || 'dev',
-  cors: {
-    origins: '*',
-    methods: '*',
-    allowedHeaders: '*',
-    exposedHeaders: 'X-Access-Token',
-    credentials: true,
-    maxAge: 1800,
-    securityOption: {
-      permitAll: ['/api/v1/account/register*', '/api/v1/authenticate'],
-      authenticated: ['/api/v1/**'],
-    },
-  },
   dbUrl: (process.env.TEST_DB_URL && process.env.NODE_ENV === 'test') ? process.env.TEST_DB_URL : process.env.DB_URL,
 };
