682d5fae67bec212787ca26e1cd2a3c82ad3ca49
MOROLARI <tmorolari@gmail.com>
2018-08-25 03:03:34 +0100

    feature(Send Notification): update sending notification to account for 'to' and 'from' field

--- server/src/task.js
+++ server/src/task.js
@@ -3,6 +3,15 @@ import schedule from 'node-schedule';
 import db from './db';
 import config from './config/config';
 
+const daysMap = {
+  sunday: 0,
+  monday: 1,
+  tuesday: 2,
+  wednesday: 3,
+  thursday: 4,
+  friday: 5,
+  saturday: 6,
+};
 export default class ScheduleTask {
   constructor() {
     const decoded = Buffer.from(config.accountServiceKey, 'base64').toString();
@@ -21,7 +30,8 @@ export default class ScheduleTask {
   static sendNotification(registrationToken, payload, options) {
     admin.messaging().sendToDevice(registrationToken, payload, options)
       .then()
-      .catch(() => {});
+      .catch(() => {
+      });
   }
 
   init() {
@@ -41,10 +51,31 @@ export default class ScheduleTask {
         },
       };
       if (reminders && reminders instanceof Array) {
-        for (let i = 0; i < reminders.length; i += 1) {
-          ScheduleTask.sendNotification(reminders[i].gcm.token, payload, this.options);
-        }
+        reminders.forEach((reminder) => {
+          let { from, to } = reminder;
+          from = daysMap[from.toLowerCase()];
+          to = daysMap[to.toLowerCase()];
+          if (ScheduleTask.withinDays(from, to, now.getDay())) {
+            ScheduleTask.sendNotification(reminder.gcm.token, payload, this.options);
+          }
+        });
       }
-    }).catch(() => {});
+    }).catch(() => {
+    });
+  }
+
+  static normalizeDay(value, lowerBound) {
+    let result = value;
+    if (result < lowerBound) {
+      result = 6 + result + 1;
+    }
+    return result;
+  }
+
+  static withinDays(from, to, day) {
+    const lowerBound = from;
+    const upperBound = ScheduleTask.normalizeDay(to, lowerBound);
+    const test = ScheduleTask.normalizeDay(day, lowerBound);
+    return test >= lowerBound && test <= upperBound;
   }
 }
