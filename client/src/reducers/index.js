b93f97d24de91a2a9b7f17f87ed361749c50000a
Morolari Titilope <tmorolari@gmail.com>
2018-11-25 11:39:03 +0100

    ft(ui): Create a login page
    - Add a login page
    - Add test to all components
    - Update babel configuration to fix coverage issues on backend api
    
    [Finishes #162149741]

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,12 +2,14 @@ import { combineReducers } from 'redux';
 import toolbar from './toolbar';
 import notification from './notification';
 import { authenticate, loading } from './authUserReducer';
+import redirect from './redirect';
 
 const appReducer = combineReducers({
   toolbar,
   notification,
   loading,
-  authenticate
+  authenticate,
+  redirect
 });
 
 export default appReducer;
